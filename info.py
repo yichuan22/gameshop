fd6352d69365ee784bb89211033adf48742ef973
yichuan22 <144292134+yichuan22@users.noreply.github.com>
2023-11-27 22:01:39 -0500

    Add files via upload

---
+++ info.py
@@ -0,0 +1,155 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "images/profile.jpeg"
+about_me = "I'm a first-year Electrical Engineering student at Georgia Tech, eager to explore and contribute to the field of technology."
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/yichuan-liu-44b0a1257/"
+my_github_url = "https://github.com/yichuan22"  
+my_email_address = "yliu3655@gatech.edu"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Electrical Engineering',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': 'April 2027',
+    'GPA': '4.0'
+}
+
+education_data1 ={
+    'Degree': 'High School Diploma "Abitur"',
+    'Institution': 'Landesgymnasium für Hochbegabte',
+    'Location': 'Schwäbisch Gmüund, BW, Germany',
+    'Graduation Date': 'July 2023',
+    'GPA': '4.0'
+}
+
+course_data = {
+    "code":["CS 1301", "MATH 1554", "PHYS 2211", "ECE 2020"], 
+    "names":["Intro to CS", "Linear Algebra", "Introductory Physics I", "Digital System Design"], 
+    "semester_taken":["1st", "2nd", "2nd", "2nd"],
+    "skills":["Python", "-", "-", "-"],
+    }
+
+experience_data = {
+    "Engineer at the largest German Aerospace Center \"DLR-Lampoldshausen\"": (
+        [
+            "- Designed and successfully launched a rocket reaching an altitude of 170 meters",
+            "- Worked under the mentorship of seasoned professionals in aerospace engineering",
+            "- Applied principles of physics to predict and analyze flight dynamics",
+            "- Utilized advanced mathematics to calculate trajectories, fuel requirements, and structural loads"
+        ],
+        "images/lampodshausen.png"
+    ),
+    "Co-Researcher: Self-Sufficient Home Project": (
+        [
+            "- Visited summer camp Juniversity with a friend",
+            "- Collaborated with a professional architect",
+            "- Developed a prototype for a self-sufficient home to combat climate change",
+            "- Presented the work to an audience of over 500 people"
+        ],
+        None
+    ),
+    "Intern and Research Assistant at Einstein E-Tech GmbH": (
+        [
+            "- Internship during 11th grade, focusing on automotive radar sensor development",
+            "- Captured and analyzed radar data",
+            "- Programmed visualization tools for radar detections"
+        ],
+        None
+    ),
+    "Researcher: Language Influence during National Socialism": (
+        [
+            "- Investigated the impact of language on public opinion in the Third Reich",
+            "- Initiated the project following discussions with Ukrainian students",
+            "- Developed a website to disseminate findings",
+            "- Conducted an interview with Holocaust survivor Abba Naor",
+            "- Visited museums and the Dachau concentration camp for in-depth research"
+        ],
+        None
+    )
+}
+
+projects_data = {
+    "Binary Adder": "Developed and presented a research paper on a binary adder system, achieving 3rd prize at 'Jugend forscht'. Personally soldered the adder, capable of summing up to 14, demonstrating a deep understanding of digital logic gates. Received the German National Physics Award 'DPG-Abiturpreis' for physics excellence.",
+    "Blow-out Lamp": "Engineered a blow out lamp featuring a circuit with a light sensor. The design effectively minimized resistance when exposed to direct light, simulating a realistic 'blow out' effect.",
+    "Discovery Project: Distacne Sensor for Safety": "Created a distance-measuring device using Arduino, ultrasonic sensors, and a speaker. The system emits a sound when an object is detected within a user-adjustable range.",
+    "CS 1301 Project: Web App": "-"
+}
+
+programming_data = {
+    "Python": 60,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+}
+spoken_icons = {
+    "German": "🇩🇪",
+    "Mandarin": "🇨🇳",
+    "English": "🇺🇸",
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "German": "Fluent",
+    "Mandarin": "Fluent",
+    "Englisch": "Fluent",
+}
+
+leadership_data = {
+    "Advanced Mathematics Co-President": (
+        [
+            "- 2nd semester of university",
+            "- Visited Lyzeum camps",
+            "- Created curriculum",
+            "- Organized and created own competitions",
+            "- Prepared students for competitions"
+        ],
+        None
+    ),
+    "Basketball for Senior Classes": (
+        [
+            "- Learned and taught tactics & basics",
+            "- Organized the school internal sport event LGH Liga",
+            "- Increased participants by 5",
+            "- Presented course to 200+ people"
+        ],
+        None 
+    ),
+    "Applied Physics Projects": (
+        [
+            "- Presented research papers on binary adder, achieving 3rd prize in the largest European competition 'Jugend forscht'",
+            "- Raised $1500 of funding for equipment and coordinated physics laboratory to instruct 30 students; built binary adder",
+            "- Obtained German National Physics Award 'DPG-Abiturpreis'"
+        ],
+        None
+    ),
+        "First Year Leadership": (
+        [
+            "- Coordinated and led philanthropy events, fostering community engagement",
+        ],
+        None 
+    )
+
+}
+
+activity_data = {
+    "German for Ukrainians": [
+        "- Created Curriculum",
+        "- Led intensive courses up to 2 hours a day",
+        "- Helped students with their daily life",
+        "- 11 Students passed the B2 Goethe Exam"
+    ],
+}
+
